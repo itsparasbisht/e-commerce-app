b3f848e3c67913cc6389d66d242f905d13617bf8
itsparasbisht <iamparasbisht@gmail.com>
2022-11-17 20:01:31 +0530

    components

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Product, FooterBanner, HeroBanner } from "../components";
 
 const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Paintings</h2>
         <p>Arts you will love to take home</p>
       </div>
-      <div>{["Painting1", "Painting2"].map((painting) => painting)}</div>
+      <div className="products-container">
+        {["Painting1", "Painting2"].map((painting) => painting)}
+      </div>
       Footer
     </>
   );
