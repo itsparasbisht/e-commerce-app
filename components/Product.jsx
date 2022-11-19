71524fa0eba8a230735445b3b81ca955b45ea3fe
itsparasbisht <iamparasbisht@gmail.com>
2022-11-19 13:53:57 +0530

    product page

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,7 +1,25 @@
+import Link from "next/link";
 import React from "react";
+import { urlFor } from "../lib/client";
 
-const Product = () => {
-  return <div>Product</div>;
+const Product = ({ product: { image, name, slug, price } }) => {
+  return (
+    <div>
+      <Link href={`/product/${slug.current}`}>
+        <div className="product-card">
+          <img
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="producr-image"
+            style={{ objectFit: "cover", borderRadius: "10px" }}
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">Rs. {price}</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Product;
